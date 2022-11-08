69b5f2bf971b0db09b5b792487c481c5e3472a56
Ian Renosto <ian.renosto@hotmail.com>
2022-11-08 12:42:42 -0300

    Placing pieces on the board

---
+++ src/chess/pieces/Queen.java
@@ -0,0 +1,18 @@
+package chess.pieces;
+
+import boardLayer.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Queen extends ChessPiece{
+
+	public Queen(Board board, Color color) {
+		super(board, color);
+	}
+	
+	@Override
+	public String toString() {
+		return "Q";
+	}
+
+}
